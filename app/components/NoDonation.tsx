1bea40d7b0ab1f16656606a0a401576553078d51
dogomayana <thomasstephen252@gmail.com>
2024-08-30 01:12:45 +0100

    No donation and download from user

---
+++ app/components/NoDonation.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function NoDonationYet() {
+  return (
+    <>
+           <div className="w-full md:w-[600px] mx-auto mt-10 p-4 rounded-md shadow-md bg-white">
+
+        <h1 className="text-lg mb-10">
+          Be part of the Bookshare campaign by donating a book
+        </h1>
+        <Link
+          href={"/pages/donateBook"}
+          className="text-base text-blue-500 block underline text-center"
+        >
+          Donate a book
+        </Link>
+      </div>
+    </>
+  );
+}
---
+++ app/components/NoDownload.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function NoDownloadYet() {
+  return (
+    <>
+      <div className="w-full md:w-[600px] mx-auto mt-10 p-4 rounded-md shadow-md bg-white">
+        <h1 className="text-lg mb-10 text-center">No Download Yet</h1>
+        <Link
+          href={"/dashboard/allBooks"}
+          className="text-base text-blue-500 block underline text-center"
+        >
+          Do you want to download a book?
+        </Link>
+      </div>
+    </>
+  );
+}
