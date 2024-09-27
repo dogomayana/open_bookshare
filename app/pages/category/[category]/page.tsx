aabefed405c313ec89d50a434716c50be31b8dbc
dogomayana <thomasstephen252@gmail.com>
2024-09-27 17:36:43 +0100

    no book popup for pages cat

--- app/pages/category/[category]/page.tsx
+++ app/pages/category/[category]/page.tsx
@@ -20,35 +20,43 @@ export default async function Page({
         <h1 className="p-3 text-gray-600 capitalize text-lg font-medium">
           <Link href={"/"}>Home </Link>/ {catData[0]?.categories}
         </h1>
-        <div className="w-full mt-2 mb-8 mx-auto p-3 grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-5">
-          {catData?.map((book: any, index: any) => (
-            <div key={index} className="block">
-              <Link href={`/pages/bookDetails/${book.slug}`}>
-                <span className="block">
-                  <img
-                    src={book.bookImage}
-                    alt="bannerImage"
-                    width={171}
-                    height={170}
-                    // style={{
-                    //   width: "100%",
-                    //   height: "100%",
-                    //   objectFit: "fill",
-                    // }}
-                    // priority={true}
-                  />
-                </span>
+        {catData.length < 1 ? (
+          <div className="grid my-8 place-items-center border border-[#0095eb]">
+            <p className="p-10 font-semibold text-base">
+              No Book for this Category
+            </p>
+          </div>
+        ) : (
+          <div className="w-full mt-2 mb-8 mx-auto p-3 grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-5">
+            {catData?.map((book: any, index: any) => (
+              <div key={index} className="block">
+                <Link href={`/pages/bookDetails/${book.slug}`}>
+                  <span className="block">
+                    <img
+                      src={book.bookImage}
+                      alt="bannerImage"
+                      width={171}
+                      height={170}
+                      // style={{
+                      //   width: "100%",
+                      //   height: "100%",
+                      //   objectFit: "fill",
+                      // }}
+                      // priority={true}
+                    />
+                  </span>
 
-                <h1 className="mt-1 text-ellipsis text-sm font-medium">
-                  {book.bookName}
-                </h1>
-                <h3 className="my-1 text-gray-500 text-sm">
-                  {book.authorName}
-                </h3>
-              </Link>
-            </div>
-          ))}
-        </div>
+                  <h1 className="mt-1 text-ellipsis text-sm font-medium">
+                    {book.bookName}
+                  </h1>
+                  <h3 className="my-1 text-gray-500 text-sm">
+                    {book.authorName}
+                  </h3>
+                </Link>
+              </div>
+            ))}
+          </div>
+        )}
       </article>
     </>
   );
